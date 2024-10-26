70623788d06f2d70b0d83592ef1fb42d9e40325d
karrk <101902544+karrk@users.noreply.github.com>
2024-10-26 15:27:10 +0900

    [Feat] 내부 데이터 테이블 저장을 위한 기능 추가

---
+++ Assets/03.PrivateFolder/JangDJ/JDJ_Scripts/DataTable.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataTable
+{
+    private Dictionary<int, List<NotePattern>> _patternTable = null;
+
+    /// <summary>
+    /// 데이터 테이블의 초기화를 진행합니다.
+    /// </summary>
+    public void Initailize()
+    {
+        _patternTable = new Dictionary<int, List<NotePattern>>();
+    }
+
+    /// <summary>
+    /// 데이터 테이블에 패턴 정보를 등록합니다.
+    /// </summary>
+    public void RegistPattern(int stageNumber, NotePattern pattern)
+    {
+        if(!_patternTable.ContainsKey(stageNumber))
+        { _patternTable.Add(stageNumber, new List<NotePattern>()); }
+
+        _patternTable[stageNumber].Add(pattern);
+    }
+
+    //public List<NotePattern> GetPattern(int patternNumber)
+    //{
+    //    return _patternTable[GameManager.Instance.StageNumber]
+    //}
+}
