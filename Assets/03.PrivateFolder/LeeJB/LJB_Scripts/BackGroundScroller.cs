37e3274e4f463f1a91e4dcb125e737ad4b29a790
LeeJeongbin <ljb97310@naver.com>
2024-10-28 20:41:36 +0900

    스크롤러 스크립트 수정

--- Assets/03.PrivateFolder/LeeJB/LJB_Scripts/BackGroundScroller.cs
+++ Assets/03.PrivateFolder/LeeJB/LJB_Scripts/BackGroundScroller.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class BackGroundScroller : MonoBehaviour
 {
-    [SerializeField] private GameObject[] tiles;        
-    [SerializeField] private float _scrollSpeed = 1.0f; // 배경 스크롤 속도
+    [SerializeField] private GameObject[] tiles;        // 타일 프리펩
+    [SerializeField] private float _scrollSpeed;        // 백그라운드 스크롤 속도
     [SerializeField] private float _tileWidth;          // 각 타일의 가로 길이
 
     private void Start()
     {
-        // 초기 타일 배치
+       // 시작 타일 배치
        // for (int i = 0; i < tiles.Length; i++)
        // {
        //     tiles[i].transform.position = new Vector3(i * _tileWidth, tiles[i].transform.position.y, tiles[i].transform.position.z);
@@ -22,14 +22,14 @@ public class BackGroundScroller : MonoBehaviour
         MoveBackground();
     }
 
-    // 배경을 왼쪽으로 이동시키고, 화면 밖으로 벗어난 타일을 재배치
+    // 백그라운드를 왼쪽으로 이동
     private void MoveBackground()
     {
         for (int i = 0; i < tiles.Length; i++)
         {
             tiles[i].transform.position += Vector3.left * _scrollSpeed * Time.deltaTime;
 
-            // 화면 밖으로 벗어난 타일을 오른쪽 끝으로 재배치
+            // 화면 밖으로 벗어난 타일을 재배치
             if (tiles[i].transform.position.x <= -_tileWidth)
             {
                 float rightMostX = GetRightMostTileX();
@@ -38,7 +38,7 @@ public class BackGroundScroller : MonoBehaviour
         }
     }
 
-    // 현재 가장 오른쪽에 있는 타일의 x 좌표를 찾음
+    // 현재 가장 오른쪽에 있는 타일의 x 좌표
     private float GetRightMostTileX()
     {
         float maxX = tiles[0].transform.position.x;
