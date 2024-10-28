7235d5b810edc1ae628be5762d1350a355469ab1
LeeJeongbin <ljb97310@naver.com>
2024-10-28 21:18:20 +0900

    스크립트 재수정

--- Assets/03.PrivateFolder/LeeJB/LJB_Scripts/BackGroundScroller.cs
+++ Assets/03.PrivateFolder/LeeJB/LJB_Scripts/BackGroundScroller.cs
@@ -10,11 +10,11 @@ public class BackGroundScroller : MonoBehaviour
 
     private void Start()
     {
-       // 시작 타일 배치
-       // for (int i = 0; i < tiles.Length; i++)
-       // {
-       //     tiles[i].transform.position = new Vector3(i * _tileWidth, tiles[i].transform.position.y, tiles[i].transform.position.z);
-       // }
+        // 시작 타일 배치
+        // for (int i = 0; i < tiles.Length; i++)
+        // {
+        //     tiles[i].transform.position = new Vector3(i * _tileWidth, tiles[i].transform.position.y, tiles[i].transform.position.z);
+        // }
     }
 
     private void Update()
@@ -22,31 +22,35 @@ public class BackGroundScroller : MonoBehaviour
         MoveBackground();
     }
 
-    // 백그라운드를 왼쪽으로 이동
+    /// <summary>
+    /// 배경 타일을 왼쪽으로 이동시키고, 위치가 벗어나면 재배치합니다.
+    /// </summary>
     private void MoveBackground()
     {
         for (int i = 0; i < tiles.Length; i++)
         {
-            tiles[i].transform.position += Vector3.left * _scrollSpeed * Time.deltaTime;
+            tiles[i].transform.position += Vector3.left * _scrollSpeed * Time.deltaTime; // 백그라운드 타일을 왼쪽으로 이동
 
-            // 화면 밖으로 벗어난 타일을 재배치
-            if (tiles[i].transform.position.x <= -_tileWidth)
+            // 타일 넓이만큼 이동 후 재배치
+            if (tiles[i].transform.position.x <= -_tileWidth) // 타일의 넓이만큼 이동 하였을 경우
             {
-                float rightMostX = GetRightMostTileX();
-                tiles[i].transform.position = new Vector3(rightMostX + _tileWidth, tiles[i].transform.position.y, tiles[i].transform.position.z);
+                float rightMost = GetRightMostTile(); // 타일의 가장 오른쪽 좌표에 타일 재배치
+                tiles[i].transform.position = new Vector3(rightMost + _tileWidth, tiles[i].transform.position.y, tiles[i].transform.position.z);
             }
         }
     }
 
-    // 현재 가장 오른쪽에 있는 타일의 x 좌표
-    private float GetRightMostTileX()
+    /// <summary>
+    /// 가장 오른쪽에 위치한 타일의 X 좌표값을 반환합니다.
+    /// </summary>
+    private float GetRightMostTile()
     {
-        float maxX = tiles[0].transform.position.x;
-        for (int i = 1; i < tiles.Length; i++)
+        float maxX = tiles[0].transform.position.x; // 타일 가장 오른쪽 부분의 X위치 변수
+        for (int i = 1; i < tiles.Length; i++) // 타일 X위치 값 업데이트
         {
             if (tiles[i].transform.position.x > maxX)
                 maxX = tiles[i].transform.position.x;
         }
-        return maxX;
+        return maxX; // X위치 값 반환
     }
 }
