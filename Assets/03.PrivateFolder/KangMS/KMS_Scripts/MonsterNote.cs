ad02948f77133e3a3c2c2bcbd7ee61b7673c3c30
Geniards <geniard93@gmail.com>
2024-10-28 13:34:22 +0900

    [Create] 몬스터 & 장애물 스크립트 생성.

---
+++ Assets/03.PrivateFolder/KangMS/KMS_Scripts/MonsterNote.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterNote : Note, IPoolingObj
+{
+    public E_Pool MyPoolType => E_Pool.MONSTER_NOTE;
+
+    public override void OnHit(E_NoteDecision decision)
+    {
+        if (!_isHit)
+        {
+            _isHit = true;
+            CalculateScore(decision);
+            ShowEffect();
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void Return()
+    {
+        ObjPoolManager.Instance.ReturnObj(MyPoolType, this.gameObject);
+    }
+}
