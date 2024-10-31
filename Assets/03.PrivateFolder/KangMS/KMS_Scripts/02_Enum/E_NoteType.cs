9812f18813c8e4781a8f3657ee336c527b46393e
Geniards <geniard93@gmail.com>
2024-10-31 16:14:27 +0900

    [Create] AnimatorData 생성(ScriptsObject)

--- Assets/03.PrivateFolder/KangMS/KMS_Scripts/02_Enum/E_NoteType.cs
+++ Assets/03.PrivateFolder/KangMS/KMS_Scripts/02_Enum/E_NoteType.cs
@@ -4,6 +4,6 @@ public enum E_NoteType
     Score,              // 점수노트
     Monster,            // 몬스터 노트
     Obstacle,           // 장애물 노트
-    ConcurrentScore,    // 동시점수 노트
+    DBScore,            // 동시점수 노트
     E_NOTETYPE_MAX
 }
